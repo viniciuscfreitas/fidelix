23c2a353730ab6a84fe4b9b845d15f8325fbde66
Vinicius do Carmo <vfreitinhas2003@gmail.com>
2024-10-21 10:59:52 -0300

    Configuração inicial do CI/CD para backend e frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Olá, PetPoints!');
